9e72c752118aa21747c2b70d88ddf7bfe4f1c5ba
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2018-08-08 10:13:18 +0300

    Remove tap import

--- firethorn/__init__.py
+++ firethorn/__init__.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 
 import config as config
 from utils import *
-from tap import *
+#from tap import *
 from models import *
 from core import *
 from pyfirethorn import *
